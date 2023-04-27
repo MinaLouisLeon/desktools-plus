71e83ae99d0ae07dcc14e38154b0db65cbf418be
MinaLouisLeon <31774352+MinaLouisLeon@users.noreply.github.com>
2023-04-27 14:16:56 +0200

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -7,7 +7,12 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyDjD5vpTKJ3PWiHshuM74kxJ6qlR4EM9fo",
+  authDomain: "desk-tools-plus.firebaseapp.com",
+  projectId: "desk-tools-plus",
+  storageBucket: "desk-tools-plus.appspot.com",
+  messagingSenderId: "555185827438",
+  appId: "1:555185827438:web:c1b1ec25a823d7595dc07f"
 };
 
 // Initialize Firebase
